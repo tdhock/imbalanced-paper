b0a7a8dc3a93fe8371bbc80cf1d1dd1395b41058
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-08-02 13:30:18 -0400

    full 864

--- 2025-07-30-conv-linear-prop0.01/results.R
+++ 2025-07-30-conv-linear-prop0.01/results.R
@@ -8,10 +8,21 @@ result.dt <- nc::capture_first_df(fread('results.csv'), learner_id=list(
   lr=".*", as.numeric
 ))[, let(
   Data=paste0("\n", task_id),
-  hours=difftime(end.time, start.time, units="hours"),
+  hours=as.numeric(difftime(end.time, start.time, units="hours")),
   start.hours=difftime(start.time, min(start.time), units="hours"),
   end.hours=difftime(end.time, min(start.time), units="hours")
+)][, let(
+  launch.i = ifelse(end.hours<20,1,2)
 )][]
+result.dt[order(hours)]
+dcast(result.dt, process ~ launch.i, length)
+(total.hours <- sum(result.dt$hours))
+hours.per.cpu <- total.hours/200
+(max.hours <- max(result.dt$hours))
+864*30
+300*30
+result.dt[order(-hours)][1:200, range(hours)]
+200*50#2.5x more efficient.
 gg <- ggplot()+
   geom_segment(aes(
     start.hours, process,
@@ -30,7 +41,18 @@ gg <- ggplot()+
   scale_fill_gradient(low="white", high="black", trans="log10")+
   scale_x_continuous("Hours from start of computation")+
   theme_bw()+
-  ggtitle("Each segment shows time for training 10000 epochs (full batch size), then re-training with best number of epochs, 543/864 jobs finished in 20 hours on 200 CPUs")
+  ggtitle("Each segment shows time for training 10000 epochs (full batch size), then re-training with best number of epochs, 864 jobs finished in 50 hours on 200 CPUs")
 png("results-timings.png", width=15, height=8, res=200, units="in")
+print(gg)
+dev.off()
+
+gg <- ggplot()+
+  ggtitle("Time distribution for training and prediction,\n864 models on MNIST-like data")+
+  geom_histogram(aes(
+    hours),
+    data=result.dt)+
+  scale_x_log10()+
+  facet_grid(arch ~ loss, labeller=label_both)
+png("results-timings-hist.png", width=6, height=3, res=200, units="in")
 print(gg)
 dev.off()
